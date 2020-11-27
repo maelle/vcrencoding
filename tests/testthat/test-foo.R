0b61bbc8712465093ba76843db5aa91cb9084afe
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-11-27 13:27:48 +0100

    add failing test

--- tests/testthat/test-foo.R
+++ tests/testthat/test-foo.R
@@ -1,3 +1,14 @@
 test_that("foo", {
   vcr::use_cassette("ropenaq-cities", ropenaq::aq_cities(page = 1))
 })
+
+test_that("Queries work with spaces and accents",{
+  vcr::use_cassette("buildQueries_accents", {
+    result1 <- ropenaq::aq_measurements(city = "Heinz+Ott", country = "DE",
+                               limit = 1, page = 1)
+    expect_is(result1, "tbl_df")
+    result2 <- ropenaq::aq_measurements(city = "J%EF%BF%BDrgen+Friesel", country = "DE",
+                               limit = 1, page = 1)
+    expect_is(result2, "tbl_df")
+  })
+})
