b614dd5932680bdd7160dbf96c27d5d943774601
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-11-27 15:33:44 +0100

    will it work with json

--- tests/testthat/setup-vcrencoding.R
+++ tests/testthat/setup-vcrencoding.R
@@ -1,7 +1,7 @@
 library("vcr") # *Required* as vcr is set up on loading
 invisible(vcr::vcr_configure(
   dir = "../fixtures",
-  serialize_with = "yaml"
+  serialize_with = "json"
 ))
 vcr::check_cassette_names()
 Sys.setenv(VCR_VERBOSE_ERRORS=TRUE)
